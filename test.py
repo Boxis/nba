82c30ca62a50a95ce9b84e1ff57d83d065ca5f0a
Boxis <zboxi7@gmail.com>
2020-02-01 11:56:07 -0500

    adding test .py

---
+++ test.py
@@ -0,0 +1,26 @@
+def remap(entry, offset):
+    if offset > 0:
+        if entry < 65:
+            return 65
+        elif entry > 89:
+            return 65
+    elif offset < 0:
+        if entry < 65:
+            return 5
+        elif entry > 89:
+            return 90
+    return entry
+
+def caesar(entry, offset):
+    result = ''
+    for letter in entry:
+        old = ord(letter)
+        med = remap(old, offset)
+        new = med + offset
+        result += chr(new)
+        print(old, med, new)
+    print(entry)
+    return(result)
+
+print(caesar('ABZ', -1))
+
