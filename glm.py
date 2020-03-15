cbe6b5a6f1b0b86bc79696483d42e49cd77366ce
hell0bye <psiops@gmail.com>
2020-03-15 12:07:14 -0400

    what is going on scitkit

--- glm.py
+++ glm.py
@@ -7,7 +7,7 @@ data = data[data.Tm != "TOT"]
 data["g_norm"] = data["G"] / 82
 data = data[~data.Pos.isin(["F", "G", "F-C", "C-F", "G-F", "F-G"])]
 
-print(data.head())
+# print(data.head())
 
 X = data[["Year","Pos","Age","height","weight","g_norm"]]
 y = data[["TS%"]]
@@ -24,7 +24,8 @@ from sklearn.model_selection import train_test_split
 
 train_X, val_X, train_y, val_y = train_test_split(X, y)
 weight = train_X["g_norm"]
-train_X.drop("g_norm",axis=1)
+train_X = train_X.drop("g_norm",axis=1)
+val_X = val_X.drop("g_norm",axis=1)
 
 object_cols = ["Pos"]
 
@@ -41,11 +42,27 @@ from sklearn.linear_model import LinearRegression
 
 model = LinearRegression()
 
-model.fit(OH_train_X, train_y,sample_weight=weight)
+model.fit(OH_train_X, train_y, sample_weight=weight)
 
-print(model.coef_)
-preds = model.predict(OH_val_X)
+print(model.intercept_, model.coef_)
+preds_val = model.predict(OH_val_X)
+preds = model.predict(OH_train_X)
 
 from sklearn.metrics import mean_absolute_error
 
-print(mean_absolute_error(val_y,preds))
+# print("MAE: " + str(mean_absolute_error(val_y,preds_val)))
+
+from p_value import pvalue
+
+# pvalue(model.intercept_, model.coef_, preds, OH_train_X, train_y)
+
+import statsmodels.api as sm
+
+X = OH_train_X
+y = train_y
+X2 = sm.add_constant(X)
+est = sm.WLS(y, X2, weights = weight)
+est2 = est.fit()
+print(est2.params)
+
+est2.predict(X2)
